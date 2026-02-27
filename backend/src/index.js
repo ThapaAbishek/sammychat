ad2873cdf164c7bafdc31ffad2289ed831c34408
ThapaAbishek <thapaa998@gmail.com>
2026-02-27 11:25:57 +0545

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,9 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
-app.get("/", (req, res) => res.send("API running ✅"));
+app.get("/", (req, res) => {
+  res.send("Backend is running ✅");
+});
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.listen(PORT, "0.0.0.0", () => {
